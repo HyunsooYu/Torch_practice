8ef02cbb02ce0ea871c4e9c158a8deb26dd6b4f8
HyunsooYu <hyunsoo940324@gmail.com>
2022-02-10 17:34:40 +0900

    Start project

--- CNN_MNIST_Classification/MNIST_classification.py
+++ CNN_MNIST_Classification/MNIST_classification.py
@@ -5,3 +5,11 @@ Created on Thu Feb 10 17:16:40 2022
 @author: BMCL
 """
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+from matplotlib import pyplot as plt
+
