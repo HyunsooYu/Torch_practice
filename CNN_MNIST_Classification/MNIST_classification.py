c3c3d8af5f288eb93d00df328b26c3b3e3f8e714
HyunsooYu <hyunsoo940324@gmail.com>
2022-02-10 17:58:52 +0900

    download MNIST data

--- CNN_MNIST_Classification/MNIST_classification.py
+++ CNN_MNIST_Classification/MNIST_classification.py
@@ -13,3 +13,21 @@ from torchvision import datasets, transforms
 
 from matplotlib import pyplot as plt
 
+is_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if is_cuda else 'cpu')
+
+print('Current cuda device is ', device)
+
+# set Hyperparameter
+
+batch_size = 50
+epoch_num = 15
+learning_rate = 0.0001
+
+# Load MNIST data
+train_data = datasets.MNIST(root = './data', train = True, download = True, transform = transforms.ToTensor())
+test_data = datasets.MNIST(root = './data', train = False, download = True, transform = transforms.ToTensor())
+
+print('number of training data: ', len(train_data))
+print('number of test data :', len(test_data))
+
