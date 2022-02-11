41ce54803ac85c4801c5d5c677f20dc31ec9deac
HyunsooYu <65939132+HyunsooYu@users.noreply.github.com>
2022-02-11 11:59:03 +0900

    Add Evaluation

--- CNN_MNIST_Classification/MNIST_classification.py
+++ CNN_MNIST_Classification/MNIST_classification.py
@@ -31,3 +31,84 @@ test_data = datasets.MNIST(root = './data', train = False, download = True, tran
 print('number of training data: ', len(train_data))
 print('number of test data :', len(test_data))
 
+# show MNIST data
+
+image, label= train_data[0]
+
+plt.imshow(image.squeeze().numpy(), cmap = 'gray')
+plt.title('label : %s' % label)
+plt.show()
+
+# Make mini batch
+
+train_loader = torch.utils.data.DataLoader(dataset = train_data, batch_size = batch_size, shuffle = True)
+test_loader = torch.utils.data.DataLoader(dataset = test_data, batch_size = batch_size, shuffle = True)
+first_batch = train_loader.__iter__().__next__()
+print('{:15s} | {:<25s} | {}'.format('name', 'type', 'size'))
+print('{:15s} | {:<25s} | {}'.format('Num of Batch', '', len(train_loader)))
+print('{:15s} | {:<25s} | {}'.format('first_batch', str(type(first_batch)), len(first_batch)))
+print('{:15s} | {:<25s} | {}'.format('first_batch[0]', str(type(first_batch[0])), len(first_batch[0]))) # image
+print('{:15s} | {:<25s} | {}'.format('first_batch[1]', str(type(first_batch[1])), len(first_batch[1]))) # label
+
+## CNN model
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32,kernel_size=3,stride=1, padding=False)
+        self.conv2 = nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3,stride=1, padding=False)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216,128)
+        self.fc2 = nn.Linear(128,10)
+        
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x,2)
+        x = self.dropout1(x)
+        x = torch.flatten(x,1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x,dim=1)
+        return output
+    
+# Optimizer & Loss function    
+model = CNN().to(device)
+optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+criterion = nn.CrossEntropyLoss()
+
+print(model)
+
+# Train
+
+model.train()
+i = 0
+for epoch in range(epoch_num):
+    for data, target in train_loader:
+        data = data.to(device)
+        target = target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if i%1000==0:
+            print('Train Step: {}\tLoss: {:.3f}'.format(i,loss.item()))
+        i+=1
+
+# Evaluation
+model.eval()
+correct = 0
+for data, target in test_loader:
+    data = data.to(device)
+    target = target.to(device)
+    output = model(data)
+    prediction = output.data.max(1)[1]
+    correct += prediction.eq(target.data).sum()
+    
+print('Test set: Accuracy: {:.2f}%'.format(100*correct/len(test_loader.dataset)))
